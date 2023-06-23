733ff420a88cc17c16c30636ddb691dcf894a19c
Mal <malachiperrin@gmail.com>
2023-06-23 13:43:53 -0400

    Refactored main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 import os
 import shutil
+from pathlib import Path
 import time
 
-
+from rich import print as rich_print
 from rich.console import Console
 from rich.table import Table
 import dotenv
@@ -15,34 +16,54 @@ from database.database_setup import setup
 dotenv.load_dotenv()
 
 
-def main() : 
-    print("Welcome to Helium!")
-    
-
+def set_path():
     confirm_path = '2'
 
     while confirm_path != '1': 
-        filepath = input("please enter file path: ")
-        confirm_path = input("Is the path correct?\nYes = 1\nNo = 2\n>> ")
+        file_path = input("please enter the file path\nEnter 3 to select from a list of files in the csv directory\n>> ")
 
-    # D:\export.csv
+        if(file_path == '3') :
+            csv_dir = Path("csv/")
+            for csv_file in csv_dir.iterdir():
+                print(format(csv_file.name))
+                
+            file_name = input("To select, enter name associated with file\n>> ")
+            file_path = os.getcwd() + "/csv/{}".format(file_name)
+        
+
+        print(file_path)    
+        confirm_path = input("Is the path correct?\nYes = 1\nNo = 2\n>> ")
 
     # Files get copied to the CSV directory for organization
 
-    filename = os.path.basename(filepath);
+    file_name = os.path.basename(file_path);
 
-    if(os.path.exists("csv/" + filename)) :
+    return file_name
+
+
+
+
+def main() : 
+    rich_print("Welcome to [bold blue]Helium![/bold blue]")
+    
+
+    file_name = set_path();
+
+
+
+
+    if(os.path.exists("csv/" + file_name)) :
         print("\nThis file already exists in the csv directory")
         time.sleep(3)
     elif(not os.path.exists(os.getcwd() + "/csv")):
         os.makedirs(os.getcwd() + "/csv")
-        shutil.copy(filepath, os.getcwd() + "/csv")
+        shutil.copy(file_path, os.getcwd() + "/csv")
     else :
-        shutil.copy(filepath, os.getcwd() + "/csv")
+        shutil.copy(file_path, os.getcwd() + "/csv")
 
     data = []
 
-    with open("csv/{}".format(filename), 'r', encoding='unicode_escape') as file:
+    with open("csv/{}".format(file_name), 'r', encoding='unicode_escape') as file:
         for (index, line) in enumerate(file):
             # data = line.readline()
 
@@ -50,6 +71,7 @@ def main() :
                 columns =  line.split(',')
             else :
                 data.append(line.split(','))
+
 
 
     table = Table(title="Data")
@@ -64,7 +86,7 @@ def main() :
     console.print(table)
 
 
-    setup(str(os.getenv("DATABASE_DRIVE")))
+    setup(str(os.getenv("DATABASE_DRIVER")))
 
 
     
