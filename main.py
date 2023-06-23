e1201e840497d92124fa1045e7326f1bbf50a7a4
Mal <malachiperrin@gmail.com>
2023-06-22 23:40:36 -0400

    csv directory is now created if it does not exist

--- main.py
+++ main.py
@@ -34,17 +34,20 @@ def main() :
     if(os.path.exists("csv/" + filename)) :
         print("\nThis file already exists in the csv directory")
         time.sleep(3)
+    elif(not os.path.exists(os.getcwd() + "/csv")):
+        os.makedirs(os.getcwd() + "/csv")
+        shutil.copy(filepath, os.getcwd() + "/csv")
     else :
         shutil.copy(filepath, os.getcwd() + "/csv")
 
     data = []
 
-    with open("csv/" + filename, 'r') as file:
+    with open("csv/{}".format(filename), 'r', encoding='unicode_escape') as file:
         for (index, line) in enumerate(file):
             # data = line.readline()
 
             if(index == 0) :
-                columns = line.split(',')
+                columns =  line.split(',')
             else :
                 data.append(line.split(','))
 
@@ -61,7 +64,7 @@ def main() :
     console.print(table)
 
 
-    setup((str.upper(os.getenv("DATABASE_DRIVER"))))
+    setup(str(os.getenv("DATABASE_DRIVE")))
 
 
     
