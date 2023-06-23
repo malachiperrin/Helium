a196205db55e7dc68286c481980afed9a33ecf8c
Mal <malachiperrin@gmail.com>
2023-06-22 23:41:26 -0400

    configured mysql connector

--- database/db_mysql.py
+++ database/db_mysql.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import errorcode
 
 import os
 import dotenv
@@ -6,6 +7,18 @@ import dotenv
 dotenv.load_dotenv()
 
 def mysqlconnect() :
-    cnx = mysql.connector.connect(user = os.getenv("DATABASE_USER"), password = os.getenv("DATABASE_PASSWORD"), host=os.getenv("DATABASE_HOST"), database=os.getenv("DATABASE_NAME"))
 
-    cnx.close()
+    try:
+        cnx = mysql.connector.connect(user = os.getenv("DATABASE_USER"), password = os.getenv("DATABASE_PASSWORD"), host=os.getenv("DATABASE_HOST"), database=os.getenv("DATABASE_NAME"))
+    
+        cnx.close()
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        cnx.close()
